2197c7adf878b18d9e0998325145b566e56dfbd2
mlechnyput <mlechnyput1@gmail.com>
2020-12-18 17:20:55 +0300

    не работает

--- src/main/java/ru/rogov/springcrud/config/HiberConfig.java
+++ src/main/java/ru/rogov/springcrud/config/HiberConfig.java
@@ -60,7 +60,7 @@ public class HiberConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "updatee");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
